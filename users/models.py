8a40caeb589d2d44cf0e89705c41507e9da852f5
Николай Зимин <zyoma@mail.ru>
2020-05-02 13:37:32 +0300

    zYoma 1_05_20

--- users/models.py
+++ users/models.py
@@ -1,20 +1,14 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 from .managers import CustomUserManager
 
 
-class User(AbstractBaseUser, PermissionsMixin):
+class User(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(default=timezone.now)
     bio = models.TextField(blank=True)
-    username = models.CharField(max_length=100, blank=True, unique=True)
-    first_name = models.CharField(max_length=100, blank=True)
-    last_name = models.CharField(max_length=100, blank=True)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     objects = CustomUserManager()
